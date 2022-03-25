6f2e063a3d30c8b45ecfd58f544e6d62e3b7a33e
rfkih <rifkihidayat23@yahoo.com>
2022-03-25 13:40:31 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = 2021;
+
+
+const userRouter = require("./src/routers/users")
+
+app.use(cors("*"));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+app.use("/users", userRouter);
+app.get("/", (req, res) => {
+  res.status(200).send("API IS RUNNING");
+});
+
+app.listen(port, (err) => {
+  if (err) return cosole.log({ err });
+
+  console.log(`Api is running at port ${port}`);
+});
